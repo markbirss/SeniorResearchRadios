801040ccc3f083337c6a469b6827885bda2cfb34
RPI 2 <771407@lcps.org>
2020-01-23 20:57:34 -0500

    SHA-1 Checksum Verification

--- 2/test.py
+++ 2/test.py
@@ -3,8 +3,10 @@ Example of library usage for streaming multiple payloads.
 """
 import time
 import board
+import hashlib
 import digitalio as dio
 from circuitpython_nrf24l01 import RF24
+
 
 # addresses needs to be in a buffer protocol object (bytearray)
 address = b'1Node'
@@ -21,6 +23,25 @@ spi = board.SPI()  # init spi bus object
 # initialize the nRF24L01 on the spi bus object
 nrf = RF24(spi, csn, ce, ard=250, arc=15, data_rate=1)
 
+def checkSHA1Checksum(l):
+    h = hashlib.new('sha1')
+    v= ''
+    
+    #Add each value in l in string form without START, END, and CHECKSUM
+    for s in l[1:-3]:
+        v = v +str(s)
+        
+    #Decode data
+    h.update(bytes(v.encode('ASCII')))
+    incoming_hash = l[l.index('Checksum')+1]
+                      
+    hash_len = len(incoming_hash)
+    generated_hash = h.hexdigest()[0:hash_len]
+    print("\nGenerated hash: " + generated_hash)
+    if (generated_hash == incoming_hash):
+        return True
+    
+
 def slave(timeout=5):
     """Stops listening after timeout with no response"""
     # set address of TX node into an RX pipe. NOTE you MUST specify
@@ -30,23 +51,35 @@ def slave(timeout=5):
     nrf.open_rx_pipe(0, address)
     nrf.listen = True  # put radio into RX mode and power up
 
-    count = 0
+    rec = []
     now = time.monotonic()  # start timer
     while time.monotonic() < now + timeout:
         if nrf.any():
-            count += 1
             # retreive the received packet's payload
             rx = nrf.recv()  # clears flags & empties RX FIFO
-            
-            #converts bytearray to normal python string
+            msg = rx.decode('ASCII')
+            rec.append(msg)
             print("Received (raw): {}".format(rx.decode('ASCII')))
+            
+            if msg == 'END' and 'BEGIN' in rec:
+                break
+                
             now = time.monotonic()
-
+            
+    #trim to correct data :]
+    useful_data = rec[rec.index('BEGIN'):]
+    if checkSHA1Checksum(useful_data) == True:
+        print('Data Integrity OK \nPROCEED')
+        #read in data for processing
+    else:
+        print('Data Integrity FAILED\nRETRY')
+        #ask transmitter to retry
+        
     # recommended behavior is to keep in TX mode while idle
     nrf.listen = False  # put the nRF24L01 is in TX mode
 
 print("""\
     nRF24L01 Stream test\n\
     Run slave() on receiver\n\
-    Run master() on transmitter""")
+    Run master() on transmitter\n""")
 slave(30)
